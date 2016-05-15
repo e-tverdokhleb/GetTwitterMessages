171ea2518c13b7bcc78add2cba0a66af7091bfdd
e-tverdokhleb <egor.tverdokhleb@gmail.com>
2016-05-15 09:10:42 +0300

    0.4

--- app/src/main/java/com/example/hp/gettwittermessages/MainActivity.java
+++ app/src/main/java/com/example/hp/gettwittermessages/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.hp.gettwittermessages;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +16,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
+        List<TwitterMessage> twitterMessageList = new ArrayList<TwitterMessage>();
+        populateRecords(twitterMessageList);
 
+        RecyclerView recycleView = (RecyclerView) findViewById(R.id.recycleView);
+        RecycleViewAdapter adapter = new RecycleViewAdapter(twitterMessageList);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        RecyclerView.ItemAnimator itemAnimatior = new DefaultItemAnimator();
+
+        recycleView.setAdapter(adapter);
+        recycleView.setLayoutManager(layoutManager);
+       recycleView.setItemAnimator(itemAnimatior);
+    }
+
+    private void populateRecords(List<TwitterMessage> twitterMessageList){
+        for (int i=0; i<10; i++){
+            TwitterMessage twitterMessage = new TwitterMessage("Good News " + String.valueOf(i),"hi  here this is good news from SpaceX",System.currentTimeMillis());
+        }
     }
 }
+
 
 
--- app/src/main/java/com/example/hp/gettwittermessages/RecycleViewActivity.java
+++ app/src/main/java/com/example/hp/gettwittermessages/RecycleViewActivity.java
@@ -2,9 +2,13 @@ package com.example.hp.gettwittermessages;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import com.example.hp.gettwittermessages.RecycleViewAdapter;
-
 import java.util.List;
+import com.example.hp.gettwittermessages.TwitterMessage;
 import java.util.ArrayList;
 
 public class RecycleViewActivity extends Activity {
@@ -12,10 +16,25 @@ public class RecycleViewActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // continue here
+        setContentView(R.layout.activity_main);
+
+        List<TwitterMessage> twitterMessageList = new ArrayList<TwitterMessage>();
+        populateRecords(twitterMessageList);
+
+        RecyclerView recycleView = (RecyclerView) findViewById(R.id.recycleView);
+        RecycleViewAdapter adapter = new RecycleViewAdapter(twitterMessageList);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        RecyclerView.ItemAnimator itemAnimatior = new DefaultItemAnimator();
+
+        recycleView.setAdapter(adapter);
+        recycleView.setLayoutManager(layoutManager);
+        recycleView.setItemAnimator(itemAnimatior);
+
     }
 
-    private void createRecords(List<TwitterMessage> twitterMessageList){
-        // continue here
+    private void populateRecords(List<TwitterMessage> twitterMessageList){
+        for (int i=0; i<10; i++){
+            TwitterMessage twitterMessage = new TwitterMessage("Good News " + String.valueOf(i),"hi  here this is good news from SpaceX",System.currentTimeMillis());
+        }
     }
 }
